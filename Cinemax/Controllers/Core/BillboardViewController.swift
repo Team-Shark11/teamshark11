67cc2856fb8033969c6e9372991d1d728d00265e
Qeshabula <umbetalyevv@gmail.com>
2023-06-27 18:26:02 +0600

    CNX-14 Sending URL Requests and Parsing JSON response

--- Cinemax/Controllers/Core/BillboardViewController.swift
+++ Cinemax/Controllers/Core/BillboardViewController.swift
@@ -12,6 +12,17 @@ class BillboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getTrendingMovies()
     }
-
+    
+    private func getTrendingMovies() {
+        APICaller.shared.getTrendingMovies { results in
+            switch results {
+            case.success(let movies):
+                print(movies)
+            case.failure(let error):
+                print(error)
+            }
+        }
+    }
 }
--- Cinemax/Models/Title.swift
+++ Cinemax/Models/Title.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Title]
+}
+
+struct Title: Codable {
+    let id: Int
+    let media_type: String?
+    let original_language: String?
+    let original_title: String?
+    let poster_path: String?
+    let release_date: String?
+    let overview: String?
+    let vote_average: Double
+    let vote_count: Int
+}
